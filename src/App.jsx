255202dff9fce4992eedf38930ae9919bac1d821
Paul Catlin <paul@ecatlin.com>
2023-04-05 08:50:56 +0100

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
-function App() {
+const pages = import.meta.glob("./pages/**/*.jsx", {eager: true});
 
-    return (
-        <div className="App">
-            <header>
-                <h1>Travel Rates Cheatsheet</h1>
-            </header>
-        </div>
-    )
+const routes = [];
+for (const path of Object.keys(pages)) {
+    const fileName = path.match(/\.\/pages\/(.*)\.jsx$/)?.[1];
+    if (!fileName) {
+        continue;
+    }
+
+    const normalizedPathName = fileName.includes("$")
+        ? fileName.replace("$", ":")
+        : fileName.replace(/\/index/, "");
+
+    routes.push({
+        path: fileName === "index" ? "/" : `/${normalizedPathName.toLowerCase()}`,
+        Element: pages[path].default,
+        loader: pages[path]?.loader,
+        action: pages[path]?.action,
+        ErrorBoundary: pages[path]?.ErrorBoundary,
+    });
 }
+
+const router = createBrowserRouter(
+    routes.map(({Element, ErrorBoundary, ...rest}) => ({
+        ...rest,
+        element: <Element/>,
+        ...(ErrorBoundary && {errorElement: <ErrorBoundary/>}),
+    }))
+);
+
+const App = () => {
+    return <RouterProvider router={router}/>;
+};
 
 export default App
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Home() {
+    return (
+        <div className="app">
+            <h1>Travel Rates Cheatsheet</h1>
+        </div>
+    );
+}
