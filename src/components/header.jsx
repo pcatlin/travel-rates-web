56ab71f971cb2197010ac3654e60cf52dbccdeb4
Paul Catlin <paul@ecatlin.com>
2023-04-06 15:26:40 +0100

    error boundary

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,14 @@
 import styles from './header.module.css';
+// import logoUrl from '../assets/icon.png';
+import logoUrl from '../assets/logo-transparent.png';
 
 export default function Header() {
     return (
         <header className={styles.header}>
-            <h2>Travel Rates Cheatsheet</h2>
+            <div className={styles.logoContainer}>
+                <img src={logoUrl} alt='Travel Rates' className={styles.logo} />
+                <h2>Travel Rates Cheatsheet</h2>
+            </div>
             <nav>
                 <a className={styles.navLink} href='/'>Home</a>
                 <a className={styles.navLink} href='/privacy'>Privacy</a>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ErrorBoundary from "./ErrorBoundary.jsx";
 import Routing from './routing.jsx';
 import './global.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Routing />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <ErrorBoundary>
+            <Routing/>
+        </ErrorBoundary>
+    </React.StrictMode>,
 )
